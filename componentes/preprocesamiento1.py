bda4d4ae46433a3c6c37643df5f603a02ee02c9e
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-04-18 18:52:14 +0200

    estructura básica

---
+++ componentes/preprocesamiento1.py
@@ -0,0 +1,36 @@
+
+def preprocesamiento1(df):
+    import pandas as pd
+    from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+    df = "" # Importar de generarModelo.py
+
+    # Escalar características
+    scaler = StandardScaler()
+    # df_mid = df.drop('Quality', axis=1).scale().as_data_frame()
+    scaler.fit(df.drop('Quality', axis=1))
+    scaled_features = scaler.transform(df.drop('Quality', axis=1))
+    df_transformado = pd.DataFrame(scaled_features, columns=df.columns[:-1])
+
+    # Codificamos la variable Quality a (1 y 0)
+    label_encoder = LabelEncoder()
+    df_transformado['Quality_encoded'] = label_encoder.fit_transform(df['Quality'])
+
+    # Eliminar valores NA
+    df_transformado.drop('Quality', axis=1)
+
+    # Eliminar valores atipicas
+     # Calcular el rango intercuartílico (IQR)
+    df_numerico = df_transformado.drop(columns=['Quality'])
+    df_numerico = df_numerico.select_dtypes(include=['number'])
+    q1 = df_numerico.quantile(0.25)
+    q3 = df_numerico.quantile(0.75)
+    iqr = q3 - q1
+
+    # Definir los límites inferior y superior para identificar valores atípicos
+    lower_limit = q1 - 1.5 * iqr
+    upper_limit = q3 + 1.5 * iqr
+
+    # Eliminar los valores atípicos
+    df_transformado = df_numerico[~((df_numerico < lower_limit) | (df_numerico > upper_limit)).any(axis=1)]
+    return df_transformado
---
+++ pipelineKubeflow.py
@@ -0,0 +1,27 @@
+import kfp
+from kfp import dsl
+from MLOpsKubeflow.componentes.preprocesamiento1 import preprocesamiento1
+from MLOpsKubeflow.componentes.preprocesamiento2 import preprocesamiento2
+from MLOpsKubeflow.componentes.modeloScoring import modeloScoring
+
+
+# Definir el pipeline principal
+@dsl.pipeline(
+    name='Predictor de calidad de plátanos',
+    description='Predice si la calidad de los plátanos son buenos o malos con un modelo de XGBoost'
+)
+def xgb_train_pipeline_local():
+    df = "hay que definirlo!!"
+    # Componente 1: Pre procesamiento 1
+    df_pre1 = preprocesamiento1(df)
+
+    # Componente 2: Pre Procesamiento 2
+    df_pre2 = preprocesamiento2(df_pre1)
+
+    # Componente 3: ModeloScoring
+    modeloScoring(df_pre2, mes="Marzo")
+
+
+# Compila y ejecuta el pipeline
+if __name__ == '__main__':
+    kfp.compiler.Compiler().compile(xgb_train_pipeline_local, 'xgb_train_pipeline_local.tar.gz')
